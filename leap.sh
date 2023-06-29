a8f65de5a37d5dc3125e3d4d38c3c595fb5bc432
Pavan9441 <anugurthi.pavan@gmail.com>
2023-06-29 21:27:18 +0000

    added leap year sctipt file

---
+++ leap.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+echo " enter first year"
+
+read first
+
+echo "enter the last year"
+
+read last
+
+for (( leap=first; leap<=last; leap++))
+do
+	if ((leap % 4==0));then 
+		echo "$leap"
+
+	fi
+done
+
+
